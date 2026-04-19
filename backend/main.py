772eaafd1f774ff981cf3282385956cb96ca72b4
Akshana <akshana.prasad.ug21@nsut.ac.in>
2026-04-20 00:54:41 +0530

    Day 2: Setup FastAPI backend with spin API and options endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+import random
 
-app = FastAPI()
+app = FastAPI(
+    title="Spin Wheel API",
+    description="backend for spin wheel game",
+    version="1.0"
+)
+
+options: list[str] = ["100", "200", "Try Again", "500", "Jackpot"]
+
+
+@app.get("/options")
+def get_options():
+    return {
+        "options": options
+    }
+
+# Sample wheel options
 
 @app.get("/")
 def home():
-    return {"message": "API is running"}
+    return {"message": "Spin Wheel API Running 🚀"}
+
+@app.get("/spin", summary="Spin the wheel and get a random result")
+def spin_wheel() -> dict:
+    result = random.choice(options)
+    return {
+        "success" : True,
+        "result": result,
+        "message": "Spin successful"
+    }
